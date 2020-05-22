ea7442fc7a37f808f032fd7a094389a743983041
Felipe-Ferraz <felipeefrz1991@gmail.com>
2020-05-22 16:35:28 -0300

    finalização do endpoint e inicio de tratamento dos erros

--- src/endpoints/friendrequest.ts
+++ src/endpoints/friendrequest.ts
@@ -1,38 +1,50 @@
-import { HashManager } from "../services/HashManager";
-import { IdGenerator } from "../services/IdGenerator";
-import { TokenManager } from '../services/TokenManager'
+import { TokenManager } from "../services/TokenManager";
 import { Request, Response } from "express";
-import { BaseDatabase } from "../data/BaseDatabase";
-import moment from 'moment';
-import { UserDatabase } from "../data/UserDatabase";
 import { FriendsDatabase } from "../data/FriendsDatabase";
+import { UserDatabase } from "../data/UserDatabase";
 
-
-export const createFriendship = async (req: Request, res: Response): Promise<void> => {
+export const createFriendship = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const token = req.headers.authorization as string;
 
     const friendData = {
-        friendReceiverId: req.body.friendReceiverId
-    }
+      friendReceiverId: req.body.friendReceiverId,
+    };
 
-    const tokenManager = new TokenManager()
+    const tokenManager = new TokenManager();
     const tokenData = tokenManager.retrieveDataFromToken(token);
 
-    const friendRequest = new FriendsDatabase();
-    const friendCheck = await friendRequest.checkFriendship(tokenData.id, friendData.friendReceiverId)
-    if(friendCheck){
-        throw new Error("Amizade ja existe.")
+    const user = await new UserDatabase().getUserId(tokenData.id);
+    const friend = await new UserDatabase().getUserId(
+      friendData.friendReceiverId
+    );
+
+    if (!user || !friend) {
+      throw new Error("Usuários não encontrado");
     }
-    await friendRequest.createFriendship(tokenData.id, friendData.friendReceiverId)
+
+    const friendRequest = new FriendsDatabase();
+    const friendCheck = await friendRequest.checkFriendship(
+      tokenData.id,
+      friendData.friendReceiverId
+    );
+    if (friendCheck) {
+      throw new Error("Amizade ja existe.");
+    }
+    await friendRequest.createFriendship(
+      tokenData.id,
+      friendData.friendReceiverId
+    );
 
     res.status(200).send({
-      sucess: "Amizade criada com sucesso!"
-    })
-
+      sucess: "Amizade criada com sucesso!",
+    });
   } catch (err) {
     res.status(402).send({
-      messager: err.message
-    })
+      messager: err.message,
+    });
   }
-}
+};
--- src/endpoints/login.ts
+++ src/endpoints/login.ts
@@ -1,43 +1,41 @@
 import { HashManager } from "../services/HashManager";
 import { UserDatabase } from "../data/UserDatabase";
-import { TokenManager } from '../services/TokenManager'
+import { TokenManager } from "../services/TokenManager";
 import { Request, Response } from "express";
 
 export const login = async (req: Request, res: Response) => {
-    try {
-        const userData = {
-            email: req.body.email,
-            password: req.body.password
-        }
+  try {
+    const userData = {
+      email: req.body.email,
+      password: req.body.password,
+    };
 
-        const userDatabase: any = new UserDatabase();
-        const user = await userDatabase.getUserEmail(userData.email);
+    const userDatabase: any = new UserDatabase();
+    const user = await userDatabase.getUserEmail(userData.email);
 
-        if(!user) {
-            throw new Error('Email invalido');
-        }
-
-        const hashManager = new HashManager();
-        const checkHash = await hashManager.compareHash(
-            userData.password,
-            user.password
-        );
-
-        if (!checkHash) {
-            throw new Error('Email ou senha incorreta.');
-          }
-
-        const tokenManager = new TokenManager()
-        const token = tokenManager.generateToken({
-            id: user.id
-        })
-
-        res.status(200).send({ token });
-
-    } catch (err) {
-        res.status(402).send({
-            message: err.message
-        });
+    if (!user) {
+      throw new Error("Email invalido");
     }
 
-}
+    const hashManager = new HashManager();
+    const checkHash = await hashManager.compareHash(
+      userData.password,
+      user.password
+    );
+
+    if (!checkHash) {
+      throw new Error("Email ou senha incorreta.");
+    }
+
+    const tokenManager = new TokenManager();
+    const token = tokenManager.generateToken({
+      id: user.id,
+    });
+
+    res.status(200).send({ token });
+  } catch (err) {
+    res.status(402).send({
+      message: err.message,
+    });
+  }
+};
